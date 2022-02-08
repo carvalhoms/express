4dd17e2e0c0185ef4ea2f427a72ba14d0eeb7fc8
Marcelo Carvalho <carvalhoms@icloud.com>
2022-02-07 21:40:07 -0300

    Criando um servidor com Express

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+	response.send("Rota usuários");
+});
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
