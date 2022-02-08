e1f1f3aa1d159ca9d43202539626db8e73e3d4cd
Marcelo Carvalho <carvalhoms@icloud.com>
2022-02-07 22:44:44 -0300

    Configurando script ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (request: Request, response: Response) => {
-	response.send("Rota usuários");
+	response.send("API Express com TtpeScript");
 });
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
