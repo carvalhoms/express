a5cbec1deae13c9257127aa78a48eaa7c53d47f4
Marcelo Carvalho <carvalhoms@icloud.com>
2022-02-07 21:58:14 -0300

    Configurando TypeScript e organizando pastas

---
+++ dist/server.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+    response.send("Rota usuários");
+});
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
